cacbaed89e8b599e8f75ef33f43bfce47bd0ee24
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-07-22 08:13:35 -0700

    Enable custom couplers by default

--- Settings.cs
+++ Settings.cs
@@ -18,12 +18,12 @@ namespace DvMod.ZRealism
         [Draw("Shunter fuel consumption multiplier")] public float shunterFuelConsumptionMultiplier = 10f;
         [Draw("Shunter oil consumption multiplier")] public float shunterOilConsumptionMultiplier = 50f;
 
-        [Draw("Use custom couplers (changes require restart) (EXPERIMENTAL)")] public bool enableCustomCouplers = false;
+        [Draw("Use custom couplers (changes require restart)")] public bool enableCustomCouplers = true;
+        [Draw("Couple on chain hooked", VisibleOn = "enableCustomCouplers|true")] public bool coupleOnChainHooked = true;
         [Draw("Coupler strength", Min = 0.1f, VisibleOn = "enableCustomCouplers|true")] public float couplerStrength = 0.85f;
         [Draw("Coupler stress smoothing", Min = 0, Max = 1, VisibleOn = "enableCustomCouplers|true")] public float couplerStressSmoothing = 0.9f;
         [Draw("Buffer spring rate", VisibleOn = "enableCustomCouplers|true")] public float bufferSpringRate = 2f;
         [Draw("Buffer damper rate", VisibleOn = "enableCustomCouplers|true")] public float bufferDamperRate = 8f;
-        [Draw("Couple on chain hooked", VisibleOn = "enableCustomCouplers|true")] public bool coupleOnChainHooked = true;
 
         [Draw("Enable logging")] public bool enableLogging = false;
         public readonly string? version = Main.mod?.Info.Version;
